6900c1f3422b19dd1794553f616b6740093b9f61
Grigory Revzin <revzingg@gmail.com>
2015-07-19 23:28:21 +0400

    Оформительское

--- servo_to_logic.sv
+++ servo_to_logic.sv
@@ -4,11 +4,11 @@
 // Обнаруживается отсутствие сигнала или его параметры неверны
 `timescale 1us / 1us
 
-module servo_to_logic(input wire servo_rise,         	// передний фронт серво
-                      input wire servo_fall,				// задний фронт серво
+module servo_to_logic(input wire servo_rise,          // передний фронт серво
+                      input wire servo_fall,          // задний фронт серво
                       input wire clk,                 // такотовый сигнал 10 кГц
-							 output logic log = 0,           // выходной логический уровень
-							 output logic rx_problem = 1);   // флаг проблемы с приёмом (нет сигнала/неправильный сигнал)
+                      output logic log = 0,           // выходной логический уровень
+                      output logic rx_problem = 1);   // флаг проблемы с приёмом (нет сигнала/неправильный сигнал)
 
 parameter MAX_HIGH_COUNT = 30;  // верхний предел длительности импульса servo = 0
 parameter MIN_HIGH_COUNT = 5;   // нижний предел длительности  импульса servo = 1
@@ -31,7 +31,6 @@ begin
  
     if (servo_rise)                            // передний фронт servo
     begin
-	     $display("On rise");
         servo_state <= 1;
         if (counter <= MIN_LOW_COUNT)          // новый сигнал пришёл сликом быстро
             rx_problem <= 1;
@@ -39,8 +38,7 @@ begin
     end
     else if (servo_fall)                       // задний фронт servo, заканчиваем счёт
     begin
-	     $display("On fall");
-	     servo_state <= 0;
+        servo_state <= 0;
         // на входе действительно управляющий сигнал сервопривода?
         if (counter > MIN_HIGH_COUNT && counter < MAX_HIGH_COUNT)
         begin
@@ -66,9 +64,6 @@ begin
         counter <= 0;
         rx_problem <= 1;
     end
- 
- 
-
 end
 
 endmodule
